290416be94297adcc882e7ec18e6b2f73b2bd865
shpomp <kur@dixa.com>
2021-11-10 17:33:15 +0100

    extracting function from component 29

--- src/components/CounterContainer.js
+++ src/components/CounterContainer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import CounterPresenter from "./CounterPresenter";
 
+const incrementUpdate = (prevState) => ({
+	counter: prevState.counter + 1,
+});
+const decrementUpdate = (prevState) => ({
+	counter: prevState.counter - 1,
+});
 class CounterContainer extends React.Component {
 	constructor(props) {
 		super(props);
@@ -9,15 +15,12 @@ class CounterContainer extends React.Component {
 		this.onDecrement = this.onDecrement.bind(this);
 	}
 	onIncrement() {
-		this.setState((prevState) => ({
-			counter: prevState.counter + 1,
-		}));
+		this.setState(incrementUpdate);
 	}
 	onDecrement() {
-		this.setState((prevState) => ({
-			counter: prevState.counter - 1,
-		}));
+		this.setState(decrementUpdate);
 	}
+
 	render() {
 		return (
 			<CounterPresenter
