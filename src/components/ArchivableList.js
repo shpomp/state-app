e42ac5018b8fb7201ab64cd833c55d66254b8a63
shpomp <kur@dixa.com>
2021-11-11 12:12:29 +0100

    ArchivableList 30

---
+++ src/components/ArchivableList.js
@@ -0,0 +1,39 @@
+import React from "react";
+class ArchiveableList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { archivedItems: [] };
+		this.onArchive = this.onArchive.bind(this);
+	}
+	onArchive(id) {
+		const { archivedItems } = this.state;
+		this.setState({
+			archivedItems: [...archivedItems, id],
+		});
+	}
+	render() {
+		const { list } = this.props;
+		const { archivedItems } = this.state;
+		const filteredList = list.filter(byArchived(archivedItems));
+		return (
+			<ul>
+				{filteredList.map((item) => (
+					<li key={item.id}>
+						<span>{item.name}</span>
+						<span>
+							<button type="button" onClick={() => this.onArchive(item.id)}>
+								Archive
+							</button>
+						</span>
+					</li>
+				))}
+			</ul>
+		);
+	}
+}
+function byArchived(archivedItems) {
+	return function (item) {
+		return !archivedItems.includes(item.id);
+	};
+}
+export default ArchiveableList;
